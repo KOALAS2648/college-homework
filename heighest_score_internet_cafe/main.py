c6428e3c2cdf0474b02095a51ca2a3b79eee6179
KOALAS2648 <Koalak1d361@gmail.com>
2025-10-21 12:13:32 +0100

    finally done

--- heighest_score_internet_cafe/main.py
+++ heighest_score_internet_cafe/main.py
@@ -1,3 +1,4 @@
+import os
 def get_data(filename):
     file_data = list(open(filename, "r"))
     #print(file_data)
@@ -9,22 +10,24 @@ def get_data(filename):
         game_data[values[0]] = {"game":values[1], "score":int(values[2])}
     return game_data
 
-def get_heighest_score_for_games(data):
-    game_score_data = {"Minecraft":{"scores":[], "players":[]},"Fortnite":{"scores":[], "players":[]},"FIFA":{"scores":[], "players":[]}}
+def get_scores_for_games(data):
+    game_score_data = {"Minecraft":{"scores":[],"highscore":0, "players":[]},"Fortnite":{"scores":[], "highscore":0,"players":[]},"FIFA":{"scores":[], "highscore":0,"players":[]}}
     for value in data:
         j = data[value]["game"]
         game_score_data[j]["players"].append(value)
         game_score_data[j]["scores"].append(data[value]["score"])
-        game_score_data[j]["scores"] = [max(game_score_data[j]["scores"])]
+        game_score_data[j]["highscore"] = max(game_score_data[j]["scores"])
     return game_score_data
 
 
 def display_players_and_scores(data):
     for value in data:
-        print(f"{value} got {data[value]['score']} in {data[value]['game']}")
+        print(f"{value} got {data[value]['scores']} in {data[value]['game']}")
 
 def display_scores_for_game(data, gamename):
-    pass
+    for ans in data[gamename]["scores"]:
+        print(f"{ans}")
+    input()
 
 def print_heightest_score(game, data):
     print(f"the players for this game : {game}")
@@ -37,17 +40,29 @@ def write_to_file(filename, dict_to_write):
     filewrite = open(filename, "w")
     for i in dict_to_write:
         temp = dict_to_write[i]
-        filewrite.write(f"{i},{temp['name']},{temp['score']}\n")
-
+        filewrite.write(f"{i},{temp['game']},{temp['score']}\n")
+def fix_server(servername):
+    data = get_data(servername)
+    write_to_file(servername, data)
 def main():
+    fix_server("server_data.txt")
     while True:
-        data = get_data("server_data.txt")
-        display_players_and_scores(data)
+        os.system("clear")
+        dataM = get_data("server_data.txt")
         ask = input("1.do you want to add your score?\n2.Do you want to view the heighest score\n3.exit the progra?(1/2/3): ")
         if ask == "1":
-            name = input("what is your name")
+            name = input("what is your name?: ")
             gamename = input("what game did you get your score in?: ")
             score = int(input("what was your score on the game?: "))
-            data[name] = {"game":gamename, "score":score}
+            dataM[name] = {"game":gamename, "score":score}
+            write_to_file("server_data.txt", dataM)
         elif ask == "2":
             game = input("what game do you wan to view the scores of?: ")
+            data = get_scores_for_games(dataM)
+            display_scores_for_game(data, game)
+        elif ask == "3":
+            break
+
+    print("thnaks for checking the internet")
+if __name__ == "__main__":
+    main()
